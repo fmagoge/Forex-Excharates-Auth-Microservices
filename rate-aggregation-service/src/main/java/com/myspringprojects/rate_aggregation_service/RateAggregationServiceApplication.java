0e6ad10228abccdb28babf350bff47d69a9b0deb
fmagoge <fmagoge@gmail.com>
2025-08-16 11:58:51 +0200

    Initial commit of the Rates Aggregator service
    This is the one expected to do must of the hearvier lifting and the major functionality sits here

---
+++ rate-aggregation-service/src/main/java/com/myspringprojects/rate_aggregation_service/RateAggregationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myspringprojects.rate_aggregation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateAggregationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateAggregationServiceApplication.class, args);
+	}
+
+}
---
+++ rate-aggregation-service/src/test/java/com/myspringprojects/rate_aggregation_service/RateAggregationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringprojects.rate_aggregation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateAggregationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
