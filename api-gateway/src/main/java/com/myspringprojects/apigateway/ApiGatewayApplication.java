8ac1f818c9c6e7cabbcacda3c85e0ff0cc699ab6
fmagoge <fmagoge@gmail.com>
2025-08-18 18:12:31 +0200

    polish up
    removing unnecessaries

--- api-gateway/src/main/java/com/myspringprojects/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/myspringprojects/apigateway/ApiGatewayApplication.java
@@ -2,14 +2,18 @@ package com.myspringprojects.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableHystrix
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
